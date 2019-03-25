52dd26fb58b2fb18df19eabd351b9025e89ef7f9
Isa Arango <farango@ideassimples.com.co>
2019-03-25 14:39:14 -0500

    Se agregan las funciones de promedio
    se agregan las funciones de promedio para
    - estudiante
    - todos

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const opn = require('opn');
+const argv = require('./modules/comands/yargs').yargs;
+const crud = require('./modules/estudiante/crud');
 
 const app = express();
-
 app.use( express.static(__dirname + '/public'));
 
-app.listen(8080);
-opn('http://localhost:8080');
+switch (argv._[0]) {
+  case 'create':
+    crud.Crear(argv);
+  break;
+
+  case 'list':
+    crud.Mostrar();
+  break;
+
+  case 'find':
+    let result = crud.Buscar(argv.nombre);
+    MostrarEstudiante(result);
+  break;
+
+  case 'prom':
+    if(argv.nombre){
+      let promedio = crud.PromedioEst(argv.nombre);
+      console.log(`El promedio del estudiante ${argv.nombre} es ${promedio}`);
+    }
+
+    if(argv.all)
+      crud.PromedioAll();
+
+  break;
+
+
+
+  default:
+    console.error('No se ingreso ningun codigo valido');
+}
+
+//app.listen(8080);
+//opn('http://localhost:8080');
